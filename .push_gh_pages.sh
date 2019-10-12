2327c3fddf984e10f6d828d30fe7ed030cf926fc
Aaron Jacobs <atheriel@gmail.com>
2019-10-12 14:06:40 -0400

    Configure Travis CI deployment.

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -7,14 +7,13 @@ GH_REPO="@github.com/$TRAVIS_REPO_SLUG.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-git config --global user.name "stephs-travis"
-git config --global user.email "steph@travis.ci"
+git config --global user.name "Aaron Jacobs"
+git config --global user.email "atheriel@gmail.com"
 
-R CMD BATCH 'ghgenerate.R'
-cp ghgenerate.Rout out
+make
 
 cd out
 git init
 git add .
-git commit -m "deployed to github pages"
+git commit -m "Updated GitHub pages."
 git push --force --quiet $FULL_REPO master:gh-pages
