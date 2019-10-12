7eb0ae6d39e3b46fab3d25683e945cd06754778f
Aaron Jacobs <atheriel@gmail.com>
2019-10-12 13:58:04 -0400

    Changes the title.

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,4 @@
-proposal<-"Boilerplate ISC Proposal"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
